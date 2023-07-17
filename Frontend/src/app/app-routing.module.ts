b936e0c6365c38bafc7a9a8d639fe22a860e8b48
superdevstar50 <superdevstar50@gmail.com>
2023-07-17 12:09:34 +0200

    Frontend: Complete Routing Module

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { EmployeeCreateComponent } from './components/employee-create/employee-create.component';
+import { EmployeeDetailComponent } from './components/employee-detail/employee-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: EmployeeListComponent
+},
+{
+  path: 'create',
+  component: EmployeeCreateComponent
+},
+{ 
+  path: 'employee/:id',
+  component: EmployeeDetailComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
