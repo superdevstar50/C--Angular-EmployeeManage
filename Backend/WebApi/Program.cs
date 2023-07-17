f5c669c3c0f83754bbefb1e8a74eb884f8e59db9
superdevstar50 <superdevstar50@gmail.com>
2023-07-17 10:25:24 +0200

    Edit Program.cs: Add Cors Policy & integrate Service

--- Backend/WebApi/Program.cs
+++ Backend/WebApi/Program.cs
@@ -1,11 +1,27 @@
+using DEmployee.ApplicationLayer.Employees;
+using DEmployee.InfrastructureLayer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+EmployeeRepository repository = new EmployeeRepository();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddScoped<IEmployeeService, EmployeeService>(f => new EmployeeService(repository));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Origins", policy =>
+    {
+        policy.AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -15,6 +31,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("Origins");
 
 app.UseAuthorization();
 
