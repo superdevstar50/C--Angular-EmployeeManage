a35d9051167c875cd8c1bd91491049b60bebeea0
superdevstar50 <superdevstar50@gmail.com>
2023-07-17 12:13:57 +0200

    Frontend: Complete App Module

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,15 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { EmployeeCreateComponent } from './components/employee-create/employee-create.component';
+import { EmployeeDetailComponent } from './components/employee-detail/employee-detail.component';
+import { SexPipe } from './utils/SexPipe';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AlertModule } from 'ngx-bootstrap/alert';
+
 @NgModule({
   declarations: [
     AppComponent,
+    EmployeeListComponent,
+    EmployeeCreateComponent,
+    EmployeeDetailComponent,
+    SexPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
