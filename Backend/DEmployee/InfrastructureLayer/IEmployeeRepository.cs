43d9daad4c9f250a1ac7c029c2c7167ed9f5f581
superdevstar50 <superdevstar50@gmail.com>
2023-07-17 08:52:19 +0200

    Add Memory Employee Repository

---
+++ Backend/DEmployee/InfrastructureLayer/IEmployeeRepository.cs
@@ -0,0 +1,21 @@
+﻿using DEmployee.Helpers.Domain;
+using DEmployee.DomainModelLayer.Employees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DEmployee.Helpers.Repository
+{
+    public interface IEmployeeRepository
+    {
+        Employee FindById(Guid id);
+
+
+        void Add(Employee entity);
+
+        void Remove(Employee entity);
+
+        IEnumerable<Employee> GetAll();
+    }
+}
---
+++ Backend/DEmployee/InfrastructureLayer/MemEmployeeRepository.cs
@@ -0,0 +1,35 @@
+﻿using DEmployee.Helpers.Domain;
+using DEmployee.Helpers.Repository;
+using DEmployee.DomainModelLayer.Employees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DEmployee.InfrastructureLayer
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        protected static List<Employee> entities = new List<Employee>();
+
+        public Employee FindById(Guid id)
+        {
+            return entities.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public void Add(Employee entity)
+        {
+            entities.Add(entity);
+        }
+
+        public void Remove(Employee entity)
+        {
+            entities.Remove(entity);
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return entities;
+        }
+    }
+}
